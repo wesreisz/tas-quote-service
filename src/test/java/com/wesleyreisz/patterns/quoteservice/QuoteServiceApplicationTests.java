731424b86722bff73da27b2e85d24318c96973d3
Wesley Reisz <wes@wesleyreisz.com>
2020-12-16 07:47:15 -0500

    Initial Project Commit of Service

---
+++ src/test/java/com/wesleyreisz/patterns/quoteservice/QuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesleyreisz.patterns.quoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
